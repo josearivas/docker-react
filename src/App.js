85284257c701a8c6c46116818a082856e0478f50
Docker <rivass.josea@gmail.com>
2019-11-05 10:51:00 +0000

    new change for test build on Travis CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New change!
+          New change again!
         </p>
         <a
           className="App-link"
